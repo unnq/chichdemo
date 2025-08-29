b27996c68de4c66d59d2b57c4cc494fdbf44e392
unnq <63836235+unnq@users.noreply.github.com>
2025-08-29 15:02:35 -0700

    Update hero3d.js

--- js/hero3d.js
+++ js/hero3d.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+
 
 (() => {
   const container = document.querySelector('.hero-3d');
