a33ec66d01495d7e05d05909fc47ed09082b31d2
unnq <63836235+unnq@users.noreply.github.com>
2025-08-29 14:30:17 -0700

    Update hero3d.js

--- js/hero3d.js
+++ js/hero3d.js
@@ -1,6 +1,6 @@
 // js/hero3d.js
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 (() => {
   const container = document.querySelector('.hero-3d');
